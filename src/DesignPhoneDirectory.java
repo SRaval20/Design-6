bc520a9c1041ab46d10ec88ce3f9e5a54a3cae5e
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-11-28 23:52:28 -0700

    Create DesignPhoneDirectory.java

---
+++ src/DesignPhoneDirectory.java
@@ -0,0 +1,35 @@
+//                         constructor       get, check, release
+// Time Complexity:           O(n)                 O(1)
+// Space Complexity:          O(n)                 O(1)
+
+public class PhoneDirectory {
+    Set<Integer> set;
+    Queue<Integer> q;
+
+    public PhoneDirectory(int maxNumbers) {
+        set = new HashSet<>();
+        q = new LinkedList<>();
+        for(int i=0; i<maxNumbers; i++) {
+            set.add(i);
+            q.add(i);
+        }
+    }
+
+    public int get() {
+        if(q.isEmpty()) return -1;                 // if no number is left
+        int next = q.poll();                       // take out next left number
+        set.remove(next);                          // make it unavailable
+        return next;
+    }
+
+    public boolean check(int number) {
+        return set.contains(number);               // if number is available
+    }
+
+    public void release(int number) {
+        if(!set.contains(number)) {                // if number not available
+            set.add(number);                       // make it
+            q.add(number);
+        }
+    }
+}
